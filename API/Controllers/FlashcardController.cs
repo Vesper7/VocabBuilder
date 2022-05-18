6d73c0db7221f7f0dcc977b8f3917dab4439f395
vesper <bartoszj7@gmail.com>
2022-05-18 20:48:03 +0200

    feat: add method to save flashcard

---
+++ API/Controllers/FlashcardController.cs
@@ -0,0 +1,48 @@
+﻿using API.Data.Configuration;
+using API.Data.DTOs;
+using API.Data.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class FlashcardController : Controller
+    {
+        private readonly DataContext _context;
+
+        public FlashcardController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("create")]
+        public async Task<IActionResult> Create(FlashcardDto flashcardDto)
+        {
+            try
+            {
+                var flashcard = new Flashcard
+                {
+                    BottomContent = flashcardDto.BottomContent,
+                    TopContent = flashcardDto.TopContent
+                };
+
+                _context.Flashcards.Add(flashcard);
+
+                await _context.SaveChangesAsync();
+
+                return new OkObjectResult(new
+                {
+                    Result = true
+                });
+            }
+            catch (Exception ex)
+            {
+                return new OkObjectResult(new
+                {
+                    Result = false,
+                    ErrorMessage = ex.Message
+                });
+            }
+        }
+    }
+}
