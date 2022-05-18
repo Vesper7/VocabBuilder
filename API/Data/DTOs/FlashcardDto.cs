6a30ff830f96a9571278dc48658584f593ca02ac
vesper <bartoszj7@gmail.com>
2022-05-18 20:47:27 +0200

    feat: add flashcardDto

---
+++ API/Data/DTOs/FlashcardDto.cs
@@ -0,0 +1,8 @@
+﻿namespace API.Data.DTOs
+{
+    public class FlashcardDto
+    {
+        public string TopContent { get; set; } = string.Empty;
+        public string BottomContent { get; set; } = string.Empty;
+    }
+}
